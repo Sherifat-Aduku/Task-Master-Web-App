068f6f87da4225e5cff6a35825c01c5d8f61a178
Sherifat-Aduku <sherifataduku.sa@gmail.com>
2025-12-04 03:11:32 +0100

    modified and crosslinked the dashboard, task and community pages. I also modified the side bar toggle

--- components/dashboard/toggle/toggle.js
+++ components/dashboard/toggle/toggle.js
@@ -1,6 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
+
+document.addEventListener("DOMContentLoaded", () => { 
     const sidebar = document.querySelector(".sidebar");
     const toggleBtn = document.getElementById("sidebarToggle");
+
+    if (!sidebar || !toggleBtn) {
+        console.error("Sidebar or toggle button not found!");
+        return;
+    }
 
     toggleBtn.addEventListener("click", () => {
         sidebar.classList.toggle("collapsed");
