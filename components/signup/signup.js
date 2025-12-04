235605e61fa9dba1cc4af87aee5cf7d9e4ff8fb7
Sherifat-Aduku <sherifataduku.sa@gmail.com>
2025-12-04 05:10:27 +0100

    cleaned up and modified the signup/login buttons

--- components/signup/signup.js
+++ components/signup/signup.js
@@ -1,9 +1,14 @@
-const loginBtn = document.getElementById("loginBtn");
-const signupBtn = document.getElementById("signupBtn");
+const loginBtn = document.getElementById("login");
+const signupBtn = document.getElementById("signup");
 
 signupBtn.addEventListener("click", () => {
   signupBtn.classList.add("active");
   loginBtn.classList.remove("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  loginBtn.classList.add("active");
+  signupBtn.classList.remove("active");
 });
 
 document.getElementById("create-btn").onclick = () => {
